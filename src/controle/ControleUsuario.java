4896ba5ed29074822a66e72cfe1f5dbc4de5fec1
Carlos <Carlos@Carlosmcdo>
2015-06-07 19:42:46 -0300

    Realizei algumas modificações para a inserção, alteração e consulta dos usuários. Mas não ta mostrando no sistema, pois temos que resolver como fazer o Controller. Abrax

--- src/controle/ControleUsuario.java
+++ src/controle/ControleUsuario.java
@@ -3,24 +3,26 @@ package controle;
 import java.sql.*;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
+import modelo.Usuario;
 
 /**
  *
- * @author itaboray
+ * @author Carlosmcdo
  */
-/*
-public class ControleFuncionario {
 
-    public boolean adicionarFuncionario(Funcionario cadfunc) {
-        String SQL = "INSERT INTO funcionario (nome,login,senha) VALUES( ?, ?, ?)";
+public class ControleUsuario {
+
+    public boolean adicionarUsuario(Usuario cadUsuario) {
+        String SQL = "INSERT INTO funcionario (nome,senha,telefone, isAdmin) VALUES(?, ?, ?, 0)";
         Connection conn = null;
         PreparedStatement pst = null;
         try {
             conn = Conexao.getConexao();
             pst = conn.prepareStatement(SQL);
-            pst.setString(1, cadfunc.getNomeFuncionario());
-            pst.setString(2, cadfunc.getLogin());
-            pst.setString(3, cadfunc.getSenha());
+            pst.setString(1, cadUsuario.getNome());
+            pst.setString(2, Integer.toString(cadUsuario.getSenha()));
+            pst.setString(3, cadUsuario.getTelefone());
+            pst.setBoolean(4,cadUsuario.isIsAdmin());
             pst.executeUpdate();
             pst = null;
         } catch (SQLException e) {
@@ -29,18 +31,20 @@ public class ControleFuncionario {
         }
         return true;
     }
-
-    public boolean alterarFuncionario(Funcionario cadfunc) {
-        String SQL = "update funcionario set nome=?,login=?,senha=? where id_funcionario=?";
+    
+    public boolean alterarUsuario(Usuario altUsuario) {
+        String SQL = "UPDATE usuario SET nome = ?, senha = ?, telefone = ?, statusU = ? "
+                     + "WHERE idUsuario = ?";
         Connection conn = null;
         PreparedStatement pst = null;
         try {
             conn = Conexao.getConexao();
             pst = conn.prepareStatement(SQL);
-            pst.setString(1, cadfunc.getNomeFuncionario());
-            pst.setString(2, cadfunc.getLogin());
-            pst.setString(3, cadfunc.getSenha());
-            pst.setInt(4, cadfunc.getIdFuncionario());
+            pst.setString (1, altUsuario.getNome());
+            pst.setString (2, Integer.toString(altUsuario.getSenha()));
+            pst.setString (3, altUsuario.getTelefone());
+            pst.setBoolean(4, altUsuario.isStatus());
+            pst.setBoolean(5, altUsuario.isIsAdmin());
             pst.executeUpdate();
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Erro na conexão ao alterar: " + e.getMessage(), "ERRO", 0);
@@ -49,40 +53,41 @@ public class ControleFuncionario {
         return true;
     }
 
-    public boolean removerFuncionario(Funcionario cadfunc) {
+    public boolean removerFuncionario(Usuario remUsuario) {
         String SQL = "delete from funcionario where id_funcionario=?";
         Connection conn = null;
         PreparedStatement pst = null;
         try {
             conn = Conexao.getConexao();
             pst = conn.prepareStatement(SQL);
-            pst.setInt(1, cadfunc.getIdFuncionario());
+            pst.setInt(1, remUsuario.getIdUsuario());
             pst.executeUpdate();
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, "Erro ao deletar! Funcionário atualmente está vinculado a uma ligação!", "ERRO", 0);
+            JOptionPane.showMessageDialog(null, "Erro ao deletar! Usuário atualmente está vinculado a uma conta!", "ERRO", 0);
             return false;
         }
-        JOptionPane.showMessageDialog(null, "Funcionário removido com sucesso!", "Remover", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Usuário removido com sucesso!", "Remover", JOptionPane.INFORMATION_MESSAGE);
         return true;
     }
 
-    public ArrayList<Funcionario> getFuncionarios() {
-        String SQL = "select id_funcionario, nome, senha,login from funcionario";
+    public ArrayList <Usuario> getUsuarios() {
+        String SQL = "SELECT idUsuario, nome, senha, telefone, statusU, isAdmin FROM usuario";
         Connection conn;
         PreparedStatement pst = null;
         ResultSet tabela;
-        ArrayList<Funcionario> fun = new ArrayList<>();
+        ArrayList <Usuario> user = new ArrayList<>();
         try {
             conn = Conexao.getConexao();
             pst = conn.prepareStatement(SQL);
             tabela = pst.executeQuery();
             while (tabela.next()) {
-                Funcionario func = new Funcionario();
-                func.setIdFuncionario(tabela.getInt(1));
-                func.setNomeFuncionario(tabela.getString(2));
-                func.setSenha(tabela.getString(3));
-                func.setLogin(tabela.getString(4));
-                fun.add(func);
+                Usuario usr = new Usuario();
+                usr.setIdUsuario(tabela.getInt(1));
+                usr.setNome(tabela.getString(3));
+                usr.setTelefone(tabela.getString(4));
+                usr.setStatus(tabela.getBoolean(5));
+                usr.setIsAdmin(tabela.getBoolean(6));
+                user.add(usr);
             }
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Erro na conexão ao consultar: " + e.getMessage(), "ERRO", 0);
@@ -95,25 +100,27 @@ public class ControleFuncionario {
                 }
             }
         }
-        return fun;
+        return user;
     }
 
-    public Funcionario getFuncionario(int id) {
-        String SQL = "select * from funcionario where id_funcionario=" + id;
+    public Usuario getUsuario(int id) {
+        String SQL = "SELECT * FROM usuario WHERE idUsuario = " + id;
         Connection conn;
         PreparedStatement pst = null;
         ResultSet tabela;
-        Funcionario fun = null;
+        Usuario user = null;
         try {
             conn = Conexao.getConexao();
             pst = conn.prepareStatement(SQL);
             tabela = pst.executeQuery();
             if (tabela.next()) {
-                fun = new Funcionario();
-                fun.setIdFuncionario(tabela.getInt(1));
-                fun.setNomeFuncionario(tabela.getString(2));
-                fun.setLogin(tabela.getString(3));
-                fun.setSenha(tabela.getString(4));
+                user = new Usuario();
+                user.setIdUsuario(tabela.getInt(1));
+                user.setNome(tabela.getString(2));
+                user.setSenha(Integer.parseInt(tabela.getString(3)));
+                user.setTelefone(tabela.getString(4));
+                user.setStatus(tabela.getBoolean(5));
+                user.setIsAdmin(tabela.getBoolean(6));
             }
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Erro na conexão ao consultar: " + e.getMessage(), "ERRO", 0);
@@ -126,38 +133,6 @@ public class ControleFuncionario {
                 }
             }
         }
-        return fun;
-    }
-
-    public Funcionario getFuncionarioCriterio(String criterio, String valor) {
-        String SQL = "select id_funcionario,nome,login,senha from funcionario where " + criterio + "=" + valor;
-        Connection conn = null;
-        PreparedStatement pst = null;
-        ResultSet tabela = null;
-        Funcionario fun = null;
-        try {
-            conn = Conexao.getConexao();
-            pst = conn.prepareStatement(SQL);
-            tabela = pst.executeQuery();
-            if (tabela.next()) {
-                fun = new Funcionario();
-                fun.setIdFuncionario(tabela.getInt(1));
-                fun.setNomeFuncionario(tabela.getString(2));
-                fun.setLogin(tabela.getString(3));
-                fun.setSenha(tabela.getString(4));
-            }
-        } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, "Erro na conexão ao consultar: " + e.getMessage(), "ERRO", 0);
-        } finally {
-            if (pst != null) {
-                try {
-                    pst.close();
-                } catch (SQLException e) {
-                    JOptionPane.showMessageDialog(null, "Erro ao fechar conexão: " + e.getMessage(), "ERRO", 0);
-                }
-            }
-        }
-        return fun;
+        return user;
     }
 }
-*/
